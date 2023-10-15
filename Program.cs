129d870afdc0478b09b64e5f851173822cbdb09a
Vlad-Eris <146423204+Vlad-Eris@users.noreply.github.com>
2023-10-15 23:41:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Adunare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, rez;
+
+            x = Convert.ToInt32(Console.ReadLine());   
+            y = Convert.ToInt32(Console.ReadLine());
+
+            rez = x + y;
+
+            Console.WriteLine(rez);
+        }
+    }
+}
